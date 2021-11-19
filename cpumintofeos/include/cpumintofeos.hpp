74b2cb0f6e1af2437e974614a23518ddf3968332
DDC <51843516+nightshade777@users.noreply.github.com>
2021-11-18 20:06:50 -0600

    Add files via upload

--- cpumintofeos/include/cpumintofeos.hpp
+++ cpumintofeos/include/cpumintofeos.hpp
@@ -1,20 +1,289 @@
-#include <eosio/eosio.hpp>
+#pragma once
 
-using namespace std;
-using namespace eosio;
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+#include <string>
+#include <math.h>
+#include <eosiolib/system.hpp>
+#include <eosiolib/crypto.hpp>
+#include <eosiolib/multi_index.hpp>
+#include <eosiolib/transaction.hpp>
+#include <eosiolib/permission.h>
+#include <eosiolib/time.hpp>
 
-CONTRACT cpumintofeos : public contract {
-  public:
-    using contract::contract;
 
-    ACTION hi(name from, string message);
-    ACTION clear();
 
-  private:
-    TABLE messages {
-      name    user;
-      string  text;
-      auto primary_key() const { return user.value; }
-    };
-    typedef multi_index<name("messages"), messages> messages_table;
-};
+namespace eosiosystem {
+   class system_contract;
+}
+
+namespace eosio {
+
+   using std::string;
+
+   
+   class [[eosio::contract("cpumintofeos")]] token : public contract {
+      public:
+         using contract::contract;
+
+
+         // mining------------------------------------------------------
+         
+        [[eosio::on_notify("eosio.token::transfer")]]
+         void claim(name from, name to, eosio::asset quantity, std::string memo);
+         
+         //standard token contract actions -----------------------------------------------
+         
+         [[eosio::action]]
+         void create( const name&   issuer,
+                      const asset&  maximum_supply);
+        
+         [[eosio::action]]
+         void issue( const name& to, const asset& quantity, const string& memo );
+
+        
+         [[eosio::action]]
+         void transfer( const name&    from,
+                        const name&    to,
+                        const asset&   quantity,
+                        const string&  memo );
+        
+         [[eosio::action]]
+         void open( const name& owner, const symbol& symbol, const name& ram_payer );
+
+
+         [[eosio::action]]
+         void open2(name user);
+
+         
+         [[eosio::action]]
+         void close( const name& owner, const symbol& symbol );
+          
+         
+         [[eosio::action]]
+         void stake (name account, asset value);
+         
+         [[eosio::action]]
+         void unstake (name account, asset value);
+
+         [[eosio::action]]
+         void delegate (name account, name receiver, asset value);
+         
+         
+         [[eosio::action]]
+         void undelegate (name account, name receiver, asset value);
+
+
+         [[eosio::action]]
+         void retire( const asset& quantity, const string& memo );
+
+
+         [[eosio::action]] 
+         void destroytoken(std::string symbol);
+
+
+         [[eosio::action]] 
+         void destroyacc(std::string symbol, name account);
+         
+         
+         static asset get_supply( const name& token_contract_account, const symbol_code& sym_code )
+         {
+            stats statstable( token_contract_account, sym_code.raw() );
+            const auto& st = statstable.get( sym_code.raw() );
+            return st.supply;
+         }
+
+         int get_prevmine( const name& token_contract_account, const symbol_code& sym_code )
+         {
+            stats statstable( token_contract_account, sym_code.raw() );
+            const auto& st = statstable.get( sym_code.raw() );
+            return st.prevmine;
+         }
+
+         int get_ctime( const name& token_contract_account, const symbol_code& sym_code )
+         {
+            stats statstable( token_contract_account, sym_code.raw() );
+            const auto& st = statstable.get( sym_code.raw() );
+            return st.creationtime;
+         }
+         
+         static asset get_balance( const name& token_contract_account, const name& owner, const symbol_code& sym_code )
+         {
+            accounts accountstable( token_contract_account, owner.value );
+            const auto& ac = accountstable.get( sym_code.raw() );
+            return ac.balance;
+         }
+         
+         static asset get_stored_balance( const name& token_contract_account, const name& owner, const symbol_code& sym_code )
+         {
+            accounts accountstable( token_contract_account, owner.value );
+            const auto& ac = accountstable.get( sym_code.raw() );
+            return ac.storebalance;
+         }
+         
+         static asset get_cpupower_balance( const name& token_contract_account, const name& owner, const symbol_code& sym_code )
+         {
+            accounts accountstable( token_contract_account, owner.value );
+            const auto& ac = accountstable.get( sym_code.raw() );
+            return ac.cpupower;
+         }
+
+         
+         int get_stake_fraction(name user){
+
+            asset supply  = get_supply(get_self(), symbol_code("CPU"));
+            asset cpu_staked = get_cpupower_balance(get_self(), user, symbol_code("CPU"));
+
+            int stakefraction = ((cpu_staked.amount * 1000000) / supply.amount);
+
+            return stakefraction;
+
+         }
+
+         bool check_cpu_balance(name user ){
+            
+            
+            asset assetname = asset(0, symbol("CPU", 4));
+
+            auto sym_code = assetname.symbol.code();
+
+            accounts accountstable( name{"cpumintofeos"}, user.value );
+            
+            auto existing = accountstable.find(sym_code.raw());
+            
+            if (existing == accountstable.end()){
+
+                return false;
+
+            }
+            
+            return true;
+         }
+              
+    asset get_issue_rate(asset supply){
+
+        asset issuereward = asset(0, symbol("CPU", 4));
+        
+        if (supply.amount/10000 <= 25920000){
+            issuereward.amount = 10*10000;}
+        
+        else if (supply.amount/10000 <= 49248000){
+            issuereward.amount = 9*10000;}
+        
+        else if (supply.amount/10000 <= 69984000){
+            issuereward.amount = 8*10000;}
+        
+        else if (supply.amount/10000 <= 88128000){
+            issuereward.amount = 7*10000;}
+        
+        else if (supply.amount/10000 <= 103680000){
+            issuereward.amount = 6*10000;}
+        
+        else if (supply.amount/10000 <= 116640000){
+            issuereward.amount = 5*10000;}
+        
+        else if (supply.amount/10000 <= 127008000){
+            issuereward.amount = 4*10000;}
+        
+        else if (supply.amount/10000 <= 134784000){
+            issuereward.amount = 3*10000;}
+        
+        else if (supply.amount/10000 <= 139968000){
+            issuereward.amount = 2*10000;}
+        
+        else if (supply.amount/10000 <= 210000000){
+            issuereward.amount = 1*10000;}
+        
+        else {issuereward.amount = 5*1000;}
+
+            return issuereward;
+
+    }
+    
+      
+      void mine(name from){
+
+        int elapsedpm = now() - get_prevmine(get_self(), symbol_code("CPU"));
+        asset balance = get_balance(get_self(),get_self(), symbol_code("CPU"));
+
+    
+        if (elapsedpm > 0){
+      
+            asset supply = get_supply(get_self(), symbol_code("CPU"));
+            asset issuerate = get_issue_rate(supply);
+            asset issue = issuerate*elapsedpm;
+
+            action(permission_level{_self, "active"_n}, "cpumintofeos"_n, "issue"_n, 
+            std::make_tuple(get_self(), issue, std::string("issue new CPU rewards"))).send();
+
+            balance = balance + issue;
+
+            stats statstable( get_self(), symbol_code("CPU").raw() );
+            auto existing = statstable.find( symbol_code("CPU").raw() );
+            check( existing != statstable.end(), "token with symbol does not exist" );
+            const auto& st = *existing;
+
+            statstable.modify( st, same_payer, [&]( auto& s ) {
+                s.prevmine = now();
+            });
+
+          }
+            std::string minemessage = "CPU mining Reward";
+
+            balance = balance/10000;
+      
+        if (balance.amount > 0){
+            action(permission_level{_self, "active"_n}, "cpumintofeos"_n, "transfer"_n, 
+            std::make_tuple(get_self(), from, balance, minemessage)).send();
+        }
+
+}
+         using create_action = eosio::action_wrapper<"create"_n, &token::create>;
+         using issue_action = eosio::action_wrapper<"issue"_n, &token::issue>;
+         using retire_action = eosio::action_wrapper<"retire"_n, &token::retire>;
+         using transfer_action = eosio::action_wrapper<"transfer"_n, &token::transfer>;
+         using open_action = eosio::action_wrapper<"open"_n, &token::open>;
+         using close_action = eosio::action_wrapper<"close"_n, &token::close>;
+      
+         struct [[eosio::table]] account {
+            asset    balance;
+            asset    storebalance; //transfer blocker var, total ecpu locked
+            asset    cpupower; //ecpu staked to bal (includes staked from others)
+            asset    unstaking;
+            uint32_t unstake_time;
+
+            uint64_t primary_key()const { return balance.symbol.code().raw(); }
+         };
+
+         struct [[eosio::table]] currency_stats {
+            asset    supply;
+            asset    max_supply;
+            name     issuer;
+            int      prevmine;
+            int      creationtime;
+            asset    totalstake;
+            uint64_t primary_key()const { return supply.symbol.code().raw(); }
+         };
+
+
+         struct [[eosio::table]] delegatecpu {
+            
+            name     recipient;
+            asset    cpupower;
+            
+
+            uint64_t primary_key()const { return recipient.value; } 
+         };
+
+
+         typedef eosio::multi_index< "accounts"_n, account > accounts;
+         typedef eosio::multi_index< "delegates"_n, delegatecpu > delegates;
+         typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+         typedef eosio::multi_index< "stat"_n, currency_stats > statsov;
+
+
+         void sub_balance( const name& owner, const asset& value );
+         void add_balance( const name& owner, const asset& value, const name& ram_payer );
+   };
+   
+} /// namespace eosio
--- cpumintofeos/src/cpumintofeos.cpp
+++ cpumintofeos/src/cpumintofeos.cpp
@@ -1,37 +1,471 @@
 #include <cpumintofeos.hpp>
 
-ACTION cpumintofeos::hi(name from, string message) {
-  require_auth(from);
+namespace eosio {
 
-  // Init the _message table
-  messages_table _messages(get_self(), get_self().value);
+void token::create( const name&   issuer,
+                    const asset&  maximum_supply )
+{
+    require_auth( get_self() );
 
-  // Find the record from _messages table
-  auto msg_itr = _messages.find(from.value);
-  if (msg_itr == _messages.end()) {
-    // Create a message record if it does not exist
-    _messages.emplace(from, [&](auto& msg) {
-      msg.user = from;
-      msg.text = message;
+    auto sym = maximum_supply.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( maximum_supply.is_valid(), "invalid supply");
+    check( maximum_supply.amount > 0, "max-supply must be positive");
+
+    stats statstable( get_self(), sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    check( existing == statstable.end(), "token with symbol already exists" );
+
+    statstable.emplace( get_self(), [&]( auto& s ) {
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer;
+       s.creationtime  = now();
+       s.prevmine = now();
+       s.totalstake = maximum_supply-maximum_supply;//initialize at zero
+       
     });
-  } else {
-    // Modify a message record if it exists
-    _messages.modify(msg_itr, from, [&](auto& msg) {
-      msg.text = message;
-    });
-  }
 }
 
-ACTION cpumintofeos::clear() {
-  require_auth(get_self());
 
-  messages_table _messages(get_self(), get_self().value);
+void token::issue( const name& to, const asset& quantity, const string& memo )
+{
+    auto sym = quantity.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
 
-  // Delete all records in _messages table
-  auto msg_itr = _messages.begin();
-  while (msg_itr != _messages.end()) {
-    msg_itr = _messages.erase(msg_itr);
-  }
+    stats statstable( get_self(), sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    check( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
+    const auto& st = *existing;
+    check( to == st.issuer, "tokens can only be issued to issuer account" );
+    
+    require_auth( st.issuer); 
+    
+    
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must issue positive quantity" );
+
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply += quantity;
+    });
+
+    add_balance( st.issuer, quantity, st.issuer );
 }
 
-EOSIO_DISPATCH(cpumintofeos, (hi)(clear))
+void token::retire( const asset& quantity, const string& memo )
+{
+    auto sym = quantity.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    stats statstable( get_self(), sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    check( existing != statstable.end(), "token with symbol does not exist" );
+    const auto& st = *existing;
+
+    require_auth( st.issuer );
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must retire positive quantity" );
+
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply -= quantity;
+    });
+
+    sub_balance( st.issuer, quantity );
+}
+
+void token::transfer( const name&    from,
+                      const name&    to,
+                      const asset&   quantity,
+                      const string&  memo )
+{
+    check( from != to, "cannot transfer to self" );
+    require_auth( from );
+    check( is_account( to ), "to account does not exist");
+    auto sym = quantity.symbol.code();
+    stats statstable( get_self(), sym.raw() );
+    const auto& st = statstable.get( sym.raw() );
+
+    require_recipient( from );
+    require_recipient( to );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must transfer positive quantity" );
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    auto payer = has_auth( to ) ? to : from;
+
+    sub_balance( from, quantity );
+    add_balance( to, quantity, payer );
+
+}
+
+void token::sub_balance( const name& owner, const asset& value ) {
+   
+      /**
+    this function has been modified from eosio.token to implement staking system
+    it blocks transacting with stored balance and balances that are undergoing the three day clear period
+    **/
+    uint32_t time_now = now();
+    uint32_t one_day_time = 60*60*24;//1 days in seconds
+   
+   
+   //WHITELIST FOR INTSTA UNSTAKE REQURED FOR CHINTAI LENDING INTEGRATION:
+   // if (owner == name{"chintailease"}){
+    //      one_day_time = 0;
+    //}
+    
+    
+    accounts from_acnts( _self, owner.value );
+
+    const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
+    check( from.balance.amount >= value.amount, "overdrawn balance" );
+
+    from_acnts.modify( from, owner, [&]( auto& a ) 
+      {
+         
+        if(time_now<(a.unstake_time+one_day_time)) //checks if anything is in process of unstaking, will stop transfer of tokens in process of unstaking
+          {
+            uint32_t time_left = round(((a.unstake_time + one_day_time) - time_now)/60); // for error msg display only
+            std::string errormsg = "Cannot send staked tokens. Tokens in withdraw will be availible in " + std::to_string(time_left) + " minute(s)";
+            check(value <= (a.balance-(a.unstaking+a.storebalance)),errormsg);
+          }
+       
+        else 
+          {
+           check(value <= (a.balance-a.storebalance),"Cannot send staked tokens");//there are no funds in process of unstaking, only stop transfer of stored funds
+          }
+         
+         a.balance -= value;
+      });
+
+}
+
+void token::add_balance( const name& owner, const asset& value, const name& ram_payer )
+{
+   accounts to_acnts( get_self(), owner.value );
+   auto to = to_acnts.find( value.symbol.code().raw() );
+   if( to == to_acnts.end() ) {
+      to_acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = value;
+        a.storebalance = (value-value); //initialiazation without needing to specify asset symbol or precision   
+        a.unstaking = (value-value);     
+        a.unstake_time = 0; 
+        a.cpupower = value-value;
+        
+      });
+   } else {
+      to_acnts.modify( to, same_payer, [&]( auto& a ) {
+        a.balance += value;
+      });
+   }
+}
+
+void token::open( const name& owner, const symbol& symbol, const name& ram_payer )
+{
+   require_auth( ram_payer );
+
+   check( is_account( owner ), "owner account does not exist" );
+
+   auto sym_code_raw = symbol.code().raw();
+   stats statstable( get_self(), sym_code_raw );
+   const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
+   check( st.supply.symbol == symbol, "symbol precision mismatch" );
+
+   accounts acnts( get_self(), owner.value );
+   auto it = acnts.find( sym_code_raw );
+   if( it == acnts.end() ) {
+      acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = asset{0, symbol};
+        a.storebalance = a.balance;
+        a.cpupower = a.balance;    
+        a.unstaking = a.balance;     
+        a.unstake_time = 0;
+      });
+   }
+}
+//simple version of open with only username as argument, user pays for ram
+void token::open2(name user)
+{
+        require_auth(user);
+
+        asset cpu = asset(0, symbol("CPU", 4));
+       
+        auto sym = cpu.symbol.code();
+
+        auto sym_code_raw = sym.raw();
+
+        accounts acnts( get_self(), user.value );
+        auto it = acnts.find( sym_code_raw );
+        if( it == acnts.end() ) {
+            acnts.emplace( user, [&]( auto& a ){
+                a.balance = cpu;
+                a.storebalance = a.balance;
+                a.cpupower = a.balance;    
+                a.unstaking = a.balance;     
+                a.unstake_time = 0;
+            });
+        }
+
+        
+    }
+
+void token::close( const name& owner, const symbol& symbol )
+{
+   require_auth( owner );
+   accounts acnts( get_self(), owner.value );
+   auto it = acnts.find( symbol.code().raw() );
+   check( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
+   check( it->balance.amount == 0, "Cannot close because the balance is not zero." );
+   check(it->cpupower.amount == 0, "Cannot close because SVX power is not zero." );
+   acnts.erase( it );
+}
+
+
+
+void token::stake(name account, asset value)
+    {
+      //stake tokens for 24 hour CPU payouts
+      require_auth(account);
+      require_recipient(account);
+
+      require_recipient(name{"cpumintofeos"});
+
+    
+      auto sym = value.symbol.code();
+      stats statstable( _self, sym.raw() );
+      const auto& st = statstable.get( sym.raw() );
+
+      uint32_t time_now = now();
+      uint32_t one_day_time = 60*60*24;
+      
+      //WHITELIST FOR INTSTA UNSTAKE REQURED FOR CHINTAI LENDING INTEGRATION:
+      //if (account == name{"chintailease"}){
+      //   one_day_time = 0;
+      //}
+      check( value.amount > 0, "must stake positive quantity" );
+      check( value.is_valid(), "invalid quantity" );
+      check( value.symbol == st.supply.symbol, "symbol precision mismatch" );
+      check( value.symbol.is_valid(), "invalid symbol name" );
+    
+      accounts to_acnts( _self, account.value );
+      auto to = to_acnts.find( value.symbol.code().raw() );
+    
+      to_acnts.modify( to, same_payer, [&]( auto& a )
+        {
+          eosio::check((value <= (a.balance-a.storebalance) ), "Cannot stake more than your unstaked balance");
+          a.storebalance += value;
+          a.cpupower += value;
+          
+          if (a.unstaking <= value){
+              a.unstaking = (value-value);
+          }
+          else{
+              a.unstaking = a.unstaking - value;
+          }
+
+          if(time_now >= (one_day_time + a.unstake_time)){
+              a.unstaking = (value - value);
+          }
+
+        });
+    
+    }
+  
+  
+  void token::unstake(name account, asset value)
+    {
+
+      /**
+      unstake tokens, needs one day to unstake
+      note: unstaking is a dummy variable and should not be used by frontend apps to display unstaking amount
+      is only valid as unstaking amount if current time is less than one day from unstake time
+      **/
+      require_auth(account);
+      require_recipient(account);
+      require_recipient(name{"cpumintofeos"});
+    
+      auto sym = value.symbol.code();
+      stats statstable( _self, sym.raw() );
+      const auto& st = statstable.get( sym.raw() );
+
+      check( value.amount > 0, "must unstake positive quantity" );
+      check( value.is_valid(), "invalid quantity" );
+      check( value.symbol == st.supply.symbol, "symbol precision mismatch" );
+      check( value.symbol.is_valid(), "invalid symbol name" );
+    
+      uint32_t time_now = now();
+      uint32_t one_day_time = 60*60*24;
+
+      //WHITELIST FOR INTSTA UNSTAKE REQURED FOR CHINTAI LENDING INTEGRATION:
+      //if (account == name{"chintailease"}){
+      //    one_day_time = 0;
+      //}
+      
+      accounts to_acnts( _self, account.value );
+      auto to = to_acnts.find( value.symbol.code().raw() );
+   
+      to_acnts.modify( to, same_payer, [&]( auto& a ) 
+        {
+        
+          check((value <= a.storebalance  ), "Cannot unstake more than your staked balance");
+          a.storebalance -= value;
+          a.cpupower -= value;
+          check(a.cpupower.amount >= 0, "ECPU Power will become below zero. Must undelegate ECPU before unstake");
+        
+          if(time_now<= (one_day_time + a.unstake_time))
+            {
+              a.unstaking += value;
+            }
+          else
+            {
+              a.unstaking = value;
+            }
+          
+          a.unstake_time = time_now;
+        
+        });
+    
+    }
+
+  void token::destroytoken(std::string symbol) {
+    require_auth(_self);
+
+    symbol_code sym(symbol);
+    stats stats_table(_self, sym.raw());
+    auto existing = stats_table.find(sym.raw());
+    check(existing != stats_table.end(), "Token with symbol does not exist");
+
+    stats_table.erase(existing);
+  }
+
+ void token::destroyacc(std::string symbol, name account) {
+    require_auth(_self);
+
+    symbol_code sym(symbol);
+    accounts accounts_table(_self, account.value);
+    const auto &row = accounts_table.get(sym.raw(), "No balance object found for provided account and symbol");
+    accounts_table.erase(row);
+  }
+  
+  void token::delegate (name account, name receiver, asset value){
+      require_auth(account);
+      check(receiver != account, "cannot delegate to self");
+      auto sym = value.symbol.code();
+      stats statstable( _self, sym.raw() );
+      const auto& st = statstable.get( sym.raw() );
+      check( value.is_valid(), "invalid quantity" );
+      check( value.symbol == st.supply.symbol, "symbol precision mismatch" );
+      check( value.symbol.is_valid(), "invalid symbol name" );
+      check( value.amount > 0, "must enter value greater than zero");
+      //remove CPU power from lender
+      
+      accounts from_acnts( _self, account.value );
+      auto to = from_acnts.find( value.symbol.code().raw() );
+      check(to!=from_acnts.end(),"Lender must initialize CPU balance RAM");
+   
+      from_acnts.modify( to, same_payer, [&]( auto& a ) 
+        {
+          check((value <= a.cpupower ), "Cannot delegate more than your svxpower");
+          a.cpupower -= value;
+        });
+      //add CPU power to borrower
+      accounts to_acnts( _self, receiver.value );
+      auto tor = to_acnts.find( value.symbol.code().raw() );
+      check(tor!= to_acnts.end(),"receiver must initialize CPU balance RAM");
+   
+      to_acnts.modify( tor, same_payer, [&]( auto& a ) 
+        {
+          a.cpupower += value;
+        });
+      
+      
+      //add borrower to delegate table of lender
+      
+      delegates delegatetable(get_self(),account.value);
+      auto tod = delegatetable.find(receiver.value);
+      if (tod == delegatetable.end()){
+      //emplace new delegation
+          delegatetable.emplace( account, [&]( auto& a ){
+                a.recipient = receiver;
+                a.cpupower = value;
+          });
+      }
+      
+      else {
+          delegatetable.modify(tod, same_payer, [&]( auto& a ){
+                a.recipient = receiver;
+                a.cpupower = a.cpupower + value;
+          });
+      }
+  }
+  void token::undelegate (name account, name receiver, asset value){
+      require_auth(account);
+      check(receiver != account, "cannot undelegate to self");
+      auto sym = value.symbol.code();
+      stats statstable( _self, sym.raw() );
+      const auto& st = statstable.get( sym.raw() );
+      check( value.is_valid(), "invalid quantity" );
+      check( value.symbol == st.supply.symbol, "symbol precision mismatch" );
+      check( value.symbol.is_valid(), "invalid symbol name" );
+      check( value.amount > 0, "must enter value greater than zero");
+      asset finalcpupower;
+      asset initialcpupower;
+      delegates delegatetable(get_self(),account.value);
+      auto tod = delegatetable.find(receiver.value);
+      check(tod != delegatetable.end(), "cannot undelegate nonexisting delegation");
+          delegatetable.modify(tod, same_payer, [&]( auto& a ){
+                initialcpupower = a.cpupower;
+                a.cpupower = a.cpupower - value;
+                finalcpupower = a.cpupower;
+          });
+      check(value <= initialcpupower, "cannot undelegate more than delegated amount");
+      if (finalcpupower.amount == 0){
+          delegatetable.erase(tod);
+      }
+      //add cpu power to lender
+      
+      accounts from_acnts( _self, account.value );
+      auto to = from_acnts.find( value.symbol.code().raw() );
+   
+      from_acnts.modify( to, same_payer, [&]( auto& a ) 
+        {
+          a.cpupower += value;
+        });
+      
+      //remove cpu power from borrower 
+      accounts to_acnts( _self, receiver.value );
+      auto tor = to_acnts.find( value.symbol.code().raw() );
+   
+      to_acnts.modify( tor, same_payer, [&]( auto& a ) 
+        {
+          a.cpupower -= value;
+        });
+     }
+
+
+    
+[[eosio::on_notify("eosio.token::transfer")]]void token::claim(name from, name to, eosio::asset quantity, std::string memo){
+
+   if (to != get_self() || from == get_self()) {
+
+         return;
+   }
+
+   check(quantity.amount == 1, "Transfer amount to mine must be equal to 0.0001 EOS");
+   
+   mine(from);
+   
+
+}
+
+
+
+} /// namespace eosio
