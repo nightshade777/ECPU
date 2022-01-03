87fc71639936b5977c9aa13308cdb1e048d036de
DDC <51843516+nightshade777@users.noreply.github.com>
2022-01-03 16:57:04 -0600

    Add files via upload

--- ecpuvotereos/include/ecpuvotereos.hpp
+++ ecpuvotereos/include/ecpuvotereos.hpp
@@ -12,10 +12,52 @@ CONTRACT ecpuvotereos : public contract {
 
     ACTION vote(name user, asset amount, name proxy);
 
+    ACTION setwinner();
+
      [[eosio::on_notify("cpumintofeos::transfer")]] void ecputransfer(name from, name to, asset quantity, std::string memo);
 
+     static asset get_balance( const name& token_contract_account, const name& owner, const symbol_code& sym_code )
+         {
+            accounts accountstable( token_contract_account, owner.value );
+            const auto& ac = accountstable.get( sym_code.raw() );
+            return ac.balance;
+         }
+
+      name find_winning_proxy(){
+
+            asset largest_delegation = asset(0, symbol("ECPU", 8)); //initialization
+            name winning_proxy;
+
+            proxies_table proxytable(get_self(), get_self().value);
+
+             for(auto it = proxytable.begin(); it != proxytable.end(); it++){
+
+                if ((it->delegated).amount > largest_delegation.amount){
+
+                    largest_delegation = (it->delegated);
+                    winning_proxy = (it->proxy);
+
+                }
+
+
+             }
+            return winning_proxy;
+
+
+      }
+
+      name get_proxy_sender(name proxy){
+
+          proxies_table proxytable(get_self(), get_self().value);
+          auto it = proxytable.find(proxy.value);
+
+          return (it->proxysender);
+
+      }
+
   private:
-    TABLE proxies { //scope contract, contract
+    
+    struct [[eosio::table]] proxies { //scope contract, contract
       name    proxy; 
       name    proxysender; // account which the proxy sends voter rewards from
       asset   delegated; //number of ECPU votes
@@ -23,9 +65,11 @@ CONTRACT ecpuvotereos : public contract {
       auto primary_key() const { return proxy.value; }
     };
     typedef multi_index<name("proxies"), proxies> proxies_table;
+
 private:
 
-    TABLE voters {
+    struct [[eosio::table]] voters {
+      
       name user;
       asset delegated;
       name proxy; 
@@ -33,6 +77,22 @@ private:
       auto primary_key() const { return user.value; }
     };
     typedef multi_index<name("voters"), voters> voters_table;
+
+    
+    struct [[eosio::table]] account {
+            asset    balance;
+            asset    delegated;  //balance of tokens able to be delegated, max is the number of tokens staked, converted to cpupower upon delegation
+                                   //(can be thought of as amount of staked tokens which have not been delegated yet)
+            asset    undelegating; //ECPU in process of clearing undelegation
+            asset    cpupower;     //ECPU tokens delegated to bal (includes delegated from others)
+                                   //(can be thought of as the sum of all ECPU delegated to this accoount)
+           
+           
+
+            uint64_t primary_key()const { return balance.symbol.code().raw(); }
+         };
+    typedef eosio::multi_index< "accounts"_n, account > accounts;
+    
 
    
 };
--- ecpuvotereos/src/ecpuvotereos.cpp
+++ ecpuvotereos/src/ecpuvotereos.cpp
@@ -1,34 +1,66 @@
 #include <ecpuvotereos.hpp>
 
 ACTION ecpuvotereos::regproxy(name user, name proxy, name proxysender) {
-  require_auth(user);
+  require_auth(get_self());
 
   //add proxy and associated proxy rewards sending account to table
+  proxies_table proxytable(get_self(), get_self().value);
+
+  auto proxy_it = proxytable.find(proxy.value);
+
+  check (proxy_it == proxytable.end(), "Error proxy account already exists");
+
+
+      proxytable.emplace( user, [&]( auto& a ){
+                a.proxy = proxy;
+                a.proxysender = proxysender;
+                a.delegated = asset(0, symbol("ECPU", 8));
+      });
 
  
 }
 
-ACTION ecpuvotereos::vote(name user, asset amount, name proxy) {
+ACTION ecpuvotereos::vote(name user, asset quantity, name proxy) {
   require_auth(user);
 
-  //use ecpu to to vote
+
+  //check if proxy is valid, (if exists)
+  proxies_table proxytable(get_self(), get_self().value);
+  auto proxy_it = proxytable.find(proxy.value);
+  check (proxy_it != proxytable.end(), "Error proxy account doesn't exist");
+
+
+  //check if user balance is atleast quantity
+   asset ecpu_balance = get_balance(name{"cpumintofeos"}, user, symbol_code("ECPU"));
+   check (quantity.amount < ecpu_balance.amount, "Cannot vote with more than balance");
+
+  
 
   voters_table votertable(get_self(), user.value);
-  auto it = votertable.find(user.value);
+  auto voter_it = votertable.find(user.value);
   
-  if (it == votertable.end()){
+  if (voter_it == votertable.end()){
 
           votertable.emplace( user, [&]( auto& a ){
                 a.user = user;
-                a.delegated = amount;
+                a.delegated = quantity;
                 a.proxy = proxy;
+          });
+
+          proxytable.modify(proxy_it, same_payer, [&]( auto& a ){
+
+              a.delegated += quantity; 
+
           });
   }
       //if proxy is the same, 
-    else if ((it->proxy) == proxy){
+    else if ((voter_it->proxy) == proxy){
+
+          //check if (userbalance - existing votes) is atleast balance
           
-          votertable.modify(it, same_payer, [&]( auto& a ){ //add votes to existing proxy
-                a.delegated += amount;
+          votertable.modify(voter_it, same_payer, [&]( auto& a ){ //add votes to existing proxy
+                check((ecpu_balance.amount - a.delegated.amount)>= quantity.amount, "not enough ECPU to add this number of votes");
+                a.delegated += quantity;
           });
 
           //add to proxt table
@@ -36,22 +68,80 @@ ACTION ecpuvotereos::vote(name user, asset amount, name proxy) {
 
     else {
 
-          votertable.modify(it, same_payer, [&]( auto& a ){ //remove votes from exisitng proxy and add votes here
+          //find the proxy user is currently voting for: it->proxy
+          //remove votes from that proxy equal to what is currently in user's votertable
+          //add "quantity" number of votes to new proxy
+          auto oldproxy_it = proxytable.find((voter_it->proxy).value);
+          
+          proxytable.modify(oldproxy_it, same_payer, [&]( auto& a ){
+
+              a.delegated -= (voter_it->delegated); 
+
+          });
+
+          proxytable.modify(proxy_it, same_payer, [&]( auto& a ){
+
+              a.delegated += quantity; 
+
+          });
+          
+
+          votertable.modify(voter_it, same_payer, [&]( auto& a ){ //remove votes from exisitng proxy and add votes here
                 a.proxy = proxy;
-                a.delegated = amount;
+                a.delegated = quantity;
           });
 
           //remove votes from proxy table and add to new prox table
     }
 
 
+    action(permission_level{_self, "active"_n}, "ecpuvotereos"_n, "setwinner"_n, 
+            std::make_tuple()).send();
+
+
   
+}
+
+
+ACTION ecpuvotereos::setwinner(){
+
+    require_auth(get_self());
+
+    name proxy = find_winning_proxy();
+    name proxysender = get_proxy_sender(proxy);
+
+
+    action(permission_level{_self, "active"_n}, "ecpulpholder"_n, "setproxy"_n, 
+            std::make_tuple(proxy,proxysender)).send();
+
+
+    
+
+
 }
 
 [[eosio::on_notify("cpumintofeos::transfer")]] void ecpuvotereos::ecputransfer(name from, name to, asset quantity, std::string memo){
 
       //find if from is a voter, else return
       //check if votes < current balance, if so remove votes to equal balance 
+
+      asset ecpu_balance = get_balance(name{"cpumintofeos"}, from, symbol_code("ECPU"));
+
+      voters_table votertable(get_self(), from.value);
+      auto it = votertable.find(from.value);
+
+      if (it == votertable.end()){
+          return;
+      }
+
+
+      if((it->delegated).amount > ecpu_balance.amount){
+
+          votertable.modify(it, same_payer, [&]( auto& a ){ //add votes to existing proxy
+                a.delegated = ecpu_balance;
+          });
+
+      }
 
 
 }
